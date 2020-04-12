55bc00ccfa24667a26f565a247186e580e50bc24
ajibolashodipo <shodipoajibola@gmail.com>
2020-04-12 16:28:24 +0100

    added iterative factorial

--- Recursion/helloRecursion.js
+++ Recursion/helloRecursion.js
@@ -16,10 +16,23 @@ function countDown(num) {
   countDown(num);
 }
 
-countDown(7);
+//countDown(7);
 
 //second recursive function
 function sumRange(num) {
   if (num === 1) return 1;
   return num + sumRange(num - 1);
 }
+
+//factorial by iterative methods
+
+const factorial = (num) => {
+  let final = 1;
+  for (let i = num; i >= 1; i--) {
+    final = i * final;
+  }
+  console.log("ajibola");
+  return final;
+};
+
+console.log(factorial(5));
