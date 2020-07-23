24a01c094120d3518f1dd50f25a421667b563667
ajibolashodipo <shodipoajibola@gmail.com>
2020-07-23 17:15:35 +0100

    added array methods

--- PhaseTwo/Arrays/Array.js
+++ PhaseTwo/Arrays/Array.js
@@ -11,8 +11,34 @@ class MyArray {
   push(item) {
     this.data[this.length] = item
     this.length++
+    return this.length
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1]
+    delete this.data[this.length - 1]
+    this.length--
+    return lastItem
+  }
+
+  delete(index) {
+    const item = this.data[index]
+    this.shiftItems(index)
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1]
+    }
+    delete this.data[this.length - 1]
+    this.length--
   }
 }
 
 const newArray = new MyArray()
+console.log(newArray.push(5))
+console.log(newArray.push("hii"))
+console.log(newArray.push("ade"))
+console.log(newArray.delete(1))
+
 console.log(newArray)
