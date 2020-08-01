c4f9c59969e54addd07bd60168e84e04297aed92
ajibolashodipo <shodipoajibola@gmail.com>
2020-08-01 18:09:25 +0100

    added append and prepend

--- PhaseTwo/LinkedLists/Linked.js
+++ PhaseTwo/LinkedLists/Linked.js
@@ -15,6 +15,13 @@
 //   }
 // }
 
+// class Node {
+//   constructor() {
+//     this.value = value
+//     this.next = null
+//   }
+// }
+
 class LinkedList {
   //pls note that the constructor is the first linked list node
   constructor(value) {
@@ -27,6 +34,7 @@ class LinkedList {
   }
 
   append(value) {
+    // const newNode = new Node(value)
     const newNode = {
       value: value,
       next: null
@@ -37,8 +45,21 @@ class LinkedList {
     this.length++
     return this
   }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    newNode.next = this.head
+
+    this.head = newNode
+    this.length++
+    return this
+  }
 }
 
 const myLinkedList = new LinkedList(10)
 
-console.log(myLinkedList.append(5))
+console.log(myLinkedList.append(12))
+console.log(myLinkedList.prepend(13))
