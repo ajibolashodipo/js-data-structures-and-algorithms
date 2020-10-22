6433e099749262d98a69d83225a9b123ecc9ee4b
ajibolashodipo <shodipoajibola@gmail.com>
2020-10-22 01:51:20 +0100

    added more methods to doubly linked lists

--- PhaseThree/dataStructures/doublyLinkedList/doublyLinkedList.js
+++ PhaseThree/dataStructures/doublyLinkedList/doublyLinkedList.js
@@ -20,6 +20,7 @@ class DoublyLinkedLists {
       this.tail = this.head
       this.length++
       //   console.log(this)
+      return this
     } else {
       //complete the mapping of the new node using the existing tail
       this.tail.next = newNode
@@ -69,11 +70,110 @@ class DoublyLinkedLists {
       return this
     }
     let oldHead = this.head
-    let newHead = oldHead.next
+    this.head = oldHead.next
     oldHead.next = null
-    newHead.prev = null
+    this.head.prev = null
     this.length--
+    console.log(this)
     return this
+  }
+
+  unshift(val) {
+    let newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+      this.length++
+      //   console.log(this)
+      return this
+    }
+    //get old head
+    let oldHead = this.head
+    //assign old head's prev to new head
+    oldHead.prev = newNode
+    //assign new head to new node
+    this.head = newNode
+    //map the next property to the old head
+    newNode.next = oldHead
+    this.length++
+    console.log(this)
+    return this
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      console.log(null)
+      return null
+    }
+
+    if (index <= this.length / 2) {
+      console.log("working from start")
+      let current = this.head
+      for (let i = 0; i < index; i++) {
+        current = current.next
+      }
+      let found = current
+      console.log(found.val)
+      return found
+    } else {
+      console.log("working from end")
+      let current = this.tail
+      for (let i = this.length - 1; i > index; i--) {
+        current = current.prev
+      }
+      let found = current
+      console.log(found.val)
+      return found
+    }
+  }
+
+  set(index, value) {
+    let current = this.get(index)
+
+    if (current) {
+      current.val = value
+      console.log(current)
+      return current
+    }
+    return null
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false
+    if (index === 0) return this.unshift(val)
+    if (index === this.length) return this.push(val)
+    //create new node
+    let newNode = new Node(value)
+    let beforeNode = this.get(index - 1)
+    let afterNode = beforeNode.next
+
+    //remap the connections
+    beforeNode.next = newNode
+    newNode.prev = beforeNode
+    newNode.next = afterNode
+    afterNode.prev = newNode
+    this.length++
+    console.log(this)
+    return true
+  }
+  remove(index) {
+    if (index < 0 || index > this.length) return false
+    if (index === 0) return this.shift(val)
+    if (index === this.length) return this.pop()
+
+    let beforeNode = this.get(index - 1)
+    let removed = beforeNode.next
+    let afterNode = beforeNode.next.next
+
+    //remap stuff
+    beforeNode.next = afterNode
+    afterNode.prev = beforeNode
+    removed.next = null
+    removed.prev = null
+    this.length--
+    console.log(removed)
+    return removed
   }
 }
 
@@ -81,4 +181,12 @@ let dll = new DoublyLinkedLists()
 dll.push(5)
 dll.push(12)
 dll.push(23)
-dll.pop()
+dll.push(5)
+
+// dll.pop()
+// dll.shift()
+// dll.unshift(100)
+dll.get(3)
+dll.set(3, "AdemolA")
+dll.insert(1, "OKEE")
+dll.remove(2)
