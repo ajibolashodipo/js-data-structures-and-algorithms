06d8729248207d1287043db40397a6a3db833470
ajibolashodipo <shodipoajibola@gmail.com>
2020-10-26 23:24:22 +0100

    added comments to string pattern algo. began sorting

--- PhaseThree/dataStructures/SearchingAlgos/stringSearch.js
+++ PhaseThree/dataStructures/SearchingAlgos/stringSearch.js
@@ -1,16 +1,21 @@
 //MINE
-function naivaStringSearch(big, sub) {
+function naiveStringSearch(big, sub) {
   let count = 0
   let temp = ""
+  //loop through first string
   for (let i = 0; i < big.length; i++) {
+      //loop through the second string
     for (let j = 0; j < sub.length; j++) {
+        //if characters in both strings match, add said xter to temp variable
       if (sub[j] == big[i + j]) {
         temp += sub[j]
+        //when temp equals substring, update count and reset temp
         if (temp == sub) {
           count++
           temp = ""
           break
         }
+        //if they dont match, brother you gotta break out of the inner loop
       } else {
         break
       }
@@ -19,7 +24,7 @@ function naivaStringSearch(big, sub) {
   return count
 }
 
-//COLT'S
+//COLT'S. got it
 
 function naiveSearch(long, short) {
   var count = 0
@@ -35,4 +40,4 @@ function naiveSearch(long, short) {
 // naiveSearch("lorie loled", "lol")
 //console.log(naiveSearch("SERENDHAHAIPITYHAHAHAHA", "HAHA"))
 
-console.log(naivaStringSearch("lorie loled", "lol"))
+console.log(naiveStringSearch("lorie loled", "lo"))
