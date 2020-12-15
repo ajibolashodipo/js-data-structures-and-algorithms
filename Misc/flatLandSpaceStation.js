05fb8afe022a72e34e81798fdc164da1fe3490fa
ajibolashodipo <shodipoajibola@gmail.com>
2020-12-15 02:42:29 +0100

    implemented a more efficient solution for hackerrank flatLand Space Station algo

--- Misc/flatLandSpaceStation.js
+++ Misc/flatLandSpaceStation.js
@@ -9,4 +9,21 @@ function flatlandSpaceStations(n, c) {
   return max
 }
 
+//more efficient solution
+
+function flatlandSpaceStations1(n, c) {
+  c.sort((a, b) => a - b)
+  let len = c.length
+  let leftmax = c[0]
+  let rightmax = n - 1 - c[len - 1]
+  let midmax = -1
+  for (let i = 0; i < c.length - 1; i++) {
+    let rw = Math.floor(0.5 * (c[i + 1] - c[i]))
+    midmax = Math.max(midmax, rw)
+  }
+  let absMax = Math.max(leftmax, rightmax, midmax)
+  return absMax
+}
+
 console.log(flatlandSpaceStations(5, [0, 4]))
+console.log(flatlandSpaceStations1(5, [0, 4]))
