c7f4ba2faf3ea25a62bc6ef8d70e3d2b2af10632
ajibolashodipo <shodipoajibola@gmail.com>
2020-12-24 23:41:04 +0100

    added comments to the recursive code

--- Phase Four/Recursion/sum.js
+++ Phase Four/Recursion/sum.js
@@ -1,13 +1,14 @@
 // O(n^2) recursive inefficient solution
-// function sum(arr) {
-//   if (arr.length == 0) return 0
-//   let sl = arr.slice(1)
-//   return arr[0] + sum(sl)
-// }
+function sum(arr) {
+  if (arr.length == 0) return 0
+  let sl = arr.slice(1)
+  return arr[0] + sum(sl)
+}
 
 //o(n) efficient solution
+//here we go through the code just once by updating the index variable as we go along. very impressive solution
 
-function sum(arr) {
+function sum1(arr) {
   return sumHelper(arr, 0)
 }
 
@@ -17,3 +18,4 @@ function sumHelper(arr, index) {
 }
 
 console.log(sum([1, 2, 3, 4, 5]))
+console.log(sum1([1, 2, 3, 4, 5]))
